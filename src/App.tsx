bee227c386bdaee4e6e81022015f848a20ee56bc
katsuya yamaguchi <y.katsuya1116@gmail.com>
2024-07-21 16:23:52 +0900

    feat: meta情報追加

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,41 @@ import PrimeFactorization from './components/PrimeFactorization';
 import PerfectNumber from './components/PerfectNumber';
 import Gcd from './components/Gcd';
 import Footer from './components/Footer';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 function App() {
   return (
     <>
-      <div className='bg-number-pattern bg-cover bg-repeat-y md:bg-inherit md:bg-repeat-x absolute top-0 w-full'>
-        <div className='flex flex-col gap-8 md:gap-16 my-8 md:my-16'>
-          <Header />
+      <HelmetProvider>
+        <Helmet>
+          <title>THE NUMBER</title>
+          <meta
+            name="description"
+            content="数字に関する色々なことができるサービスです"
+          />
+          <meta
+            name="keywords"
+            content="数字, 素数, 素因数分解, 完全数, 最大公約数"
+          />
+        </Helmet>
+        <div className='bg-number-pattern bg-cover bg-repeat-y md:bg-inherit md:bg-repeat-x absolute top-0 w-full'>
+          <div className='flex flex-col gap-8 md:gap-16 my-8 md:my-16'>
+            <Header />
 
-          <main className="main flex flex-col items-center justify-center gap-y-8 px-4 w-9/10 md:w-3/4 mt-8 md:mt-16 my-auto md:mx-auto">
-            <PrimeNumber />
+            <main className="main flex flex-col items-center justify-center gap-y-8 px-4 w-9/10 md:w-3/4 mt-8 md:mt-16 my-auto md:mx-auto">
+              <PrimeNumber />
 
-            <PrimeFactorization />
+              <PrimeFactorization />
 
-            <PerfectNumber />
+              <PerfectNumber />
 
-            <Gcd />
-          </main>
+              <Gcd />
+            </main>
 
-          <Footer />
+            <Footer />
+          </div>
         </div>
-      </div>
+      </HelmetProvider>
     </>
   );
 }
